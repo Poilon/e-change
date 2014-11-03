b07abe469959ad585cd7a5741e3f25b0e3f6bf8a
Poilon <poilon@gmail.com>
2014-11-03 15:55:42 +0100

    user id to job

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :user_jobs
-  has_many :jobs, through: :user_jobs
+  has_many :jobs
   has_many :messages
 
   validate :first_name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102194845) do
+ActiveRecord::Schema.define(version: 20141103145527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141102194845) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "address"
+    t.integer  "user_id"
   end
 
   create_table "messages", force: true do |t|
